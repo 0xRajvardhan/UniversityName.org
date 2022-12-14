187c8fca0d2690cd58330989a9e1bd27df28847c
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-12-14 21:28:56 +0530

    header section completed

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript for Toggle Menu 
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0"
+}
+
+function hideMenu() {
+    navLinks.style.right = "-250px"
+}
